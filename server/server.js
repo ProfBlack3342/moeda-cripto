691ede2409cc0ae52c270b4f64fdaaf43c3678f9
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2025-03-24 12:10:14 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
+
 const server = express();
 
 const SERVER_PORT = 5000;
