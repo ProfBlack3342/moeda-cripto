81b87d03e3dc2c02f0deeb1f32631718b08ff422
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2025-03-25 10:33:32 -0300

    Update Register.js

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -6,11 +6,15 @@ import { User } from '../Classes.js';
 const SERVER_PORT = 5000;
 
 /**
- * 
+ * Gera e retorna um bloco de código HTML que define a página de registro
  * @param {Object} userState - Um objeto contendo:
  * @param {User | null} userState.currentUser - O usuário atualmente logado, ou nulo se não estiver.
  * @param {Function} userState.changeCurrentUser - A função que altera o usuário logado atualmente.
  * @returns Uma página HTML para registrar um usuário novo
+ * 
+ * @author Eduardo Pereira Moreira <eduardopereiramoreira1995@gmail.com>
+ * @since 1.0
+ * @version 1.0
  */
 function RegisterPage({currentUser, changeCurrentUser}) {
 
