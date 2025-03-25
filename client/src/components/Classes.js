614f85228072b30202806f048c0b4ea72041b4cc
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2025-03-25 10:33:50 -0300

    Update Classes.js

--- client/src/components/Classes.js
+++ client/src/components/Classes.js
@@ -1,3 +1,10 @@
+/**
+ * Classe que define um usuario
+ * 
+ * @author Eduardo Pereira Moreira <eduardopereiramoreira1995@gmail.com>
+ * @since 1.0
+ * @version 1.0
+ */
 export class User {
     #id = -1;
 
