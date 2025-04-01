42a2353f6fccb3088f7221e968749c493b020088
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2025-04-01 12:02:53 -0300

    Update Register.js

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -26,7 +26,7 @@ function RegisterPage({currentUser, changeCurrentUser}) {
     }, []);
 
     return(
-        <> {data
+        <> {data && !currentUser
             ? <>
                 <div className="w3-container w3-padding-64 w3-white w3-border w3-border-gray" id="register">
                     <h1 className="w3-center">{data.message}</h1>
