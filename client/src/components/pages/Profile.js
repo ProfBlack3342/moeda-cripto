eb79dd2136422c198d25d9d440ebe6dfa53adf04
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2025-04-01 12:02:50 -0300

    Update Profile.js

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -26,7 +26,7 @@ function ProfilePage({currentUser, changeCurrentUser}) {
     }, []);
 
     return(
-        <> {data
+        <> {data && currentUser
             ? <>
                 <div className="w3-container w3-padding-64 w3-white w3-border w3-border-gray" id="profile">
                     <h1 className="w3-center">{data.message}</h1>
