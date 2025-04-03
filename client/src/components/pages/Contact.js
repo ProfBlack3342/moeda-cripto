7f0d2af30aece34dd745688ace6db77032fed8aa
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2025-04-03 10:56:55 -0300

    Update Contact.js

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const SERVER_PORT = 5000;
-
 /**
  * Gera e retorna um bloco de código HTML que define a página de contato
  * @returns 
@@ -11,13 +9,14 @@ const SERVER_PORT = 5000;
  * @since 1.0
  * @version 1.1
  */
-function ContactPage() {
+function ContactPage({port}) {
 
+    const PATH = `http://localhost:${port}/api/contact`;
     const [data, setData] = useState(null);
 
     useEffect(() => {
         // Requisição para a API do backend
-        axios.get(`http://localhost:${SERVER_PORT}/api/contact`).then(response => setData(response.data)).catch(error => console.error('Erro ao buscar dados:', error));
+        axios.get(PATH).then(response => setData(response.data)).catch(error => console.error('Erro ao buscar dados:', error));
     }, []);
 
     return(
