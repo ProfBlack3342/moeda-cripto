f862011976ec6413cfdbacda068f56684147102c
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2025-04-03 11:03:40 -0300

    Update Home.js

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -7,7 +7,7 @@ import axios from 'axios';
  * 
  * @author Eduardo Pereira Moreira <eduardopereiramoreira1995@gmail.com>
  * @since 1.0
- * @version 1.1
+ * @version 1.0
  */
 function HomePage({port}) {
 
