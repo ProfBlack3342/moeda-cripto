363b9437b9611ee259ca6413a125c59cc2760c72
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2025-04-04 12:17:21 -0300

    Update Register.js

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -19,6 +19,38 @@ function RegisterPage({port, currentUser, changeCurrentUser}) {
     const PATH = `http://localhost:${port}/api/register`;
     const [data, setData] = useState(null);
 
+    const [nome, setNome] = useState('');
+    const [cpf, setCpf] = useState('');
+    const [email, setEmail] = useState('');
+    const [login, setLogin] = useState('');
+    const [senha, setSenha] = useState('');
+    const [senhaC, setSenhaC] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const user = new User();
+
+
+        axios.post(PATH, {
+            'nome' : nome,
+            'cpf': cpf,
+            'email': email,
+            'login': login,
+            'senha': senha,
+            'senhaC': senhaC
+        }).then(response => {
+            user.id = response.data.id;
+            user.login = response.data.login;
+            user.senha = response.data.hash;
+            user.nome = response.data.nome;
+            user.cpf = response.data.cpf;
+            user.email = response.data.email;
+        });
+        changeCurrentUser(user);
+        document.location.href = '/';
+    };
+
     useEffect(() => {
         // Requisição para a API do backend
         axios.get(PATH).then(response => setData(response.data)).catch(error => console.error('Erro ao buscar dados:', error));
@@ -30,33 +62,33 @@ function RegisterPage({port, currentUser, changeCurrentUser}) {
                 <div className="w3-container w3-padding-64 w3-white w3-border w3-border-gray" id="register">
                     <h1 className="w3-center">{data.message}</h1>
                     <p className="w3-center">Preencha os campos abaixo para se registrar como usuário:</p>
-                    <form className='w3-padding-large' method='POST' autoComplete='off'>
+                    <form className='w3-padding-large' method='POST' autoComplete='off' onSubmit={handleSubmit}>
                         <p>
                             <label for="nome">Nome:</label>
-                            <input className="w3-input w3-padding-16" type="text" id="nome" name="nome" required/>
+                            <input className="w3-input w3-padding-16" type="text" id="nome" name="nome" onChange={(evt) => setNome(evt.target.value)} required/>
                         </p>
                         <p>
                             <label for="cpf">CPF:</label>
-                            <input className="w3-input w3-padding-16" type="text" id="cpf" name="cpf" required/>
+                            <input className="w3-input w3-padding-16" type="text" id="cpf" name="cpf" onChange={(evt) => setCpf(evt.target.value)} required/>
                         </p>
                         <p>
                             <label for="email">Email:</label>
-                            <input className="w3-input w3-padding-16" type="email" id="email" name="email" required/>
+                            <input className="w3-input w3-padding-16" type="email" id="email" name="email" onChange={(evt) => setEmail(evt.target.value)} required/>
                         </p>
                         <p>
                             <label for="login">Login:</label>
-                            <input className="w3-input w3-padding-16" type="text" id="login" name="login" required/>
+                            <input className="w3-input w3-padding-16" type="text" id="login" name="login" onChange={(evt) => setLogin(evt.target.value)} required/>
                         </p>
                         <p>
                             <label for="senha">Senha:</label>
-                            <input className="w3-input w3-padding-16" type="password" id="senha" name="senha" required/>
+                            <input className="w3-input w3-padding-16" type="password" id="senha" name="senha" onChange={(evt) => setSenha(evt.target.value)} required/>
                         </p>
                         <p>
                             <label for="senhaC">Confirme a sua Senha:</label>
-                            <input className="w3-input w3-padding-16" type="password" id="senhaC" name="senhaC" required/>  
+                            <input className="w3-input w3-padding-16" type="password" id="senhaC" name="senhaC" onChange={(evt) => setSenhaC(evt.target.value)} required/>  
                         </p>
                         <p>
-                            <button className="w3-button w3-light-grey w3-section" type="submit">Fazer Cadastro</button>
+                            <button className="w3-button w3-light-grey w3-section" type="submit" >Fazer Cadastro</button>
                         </p>
                     </form>
                 </div>
