949d2d92fe7469512731870e3d3de54bf05d2097
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2025-04-09 12:29:57 -0300

    Update Register.js

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -20,26 +20,27 @@ function RegisterPage({port, currentUser, changeCurrentUser}) {
     const PATH = `http://localhost:${port}/api/register`;
     const [data, setData] = useState(null);
 
-    const [postForm, setPostForm] = useState(false);
-    const [nome, setNome] = useState('');
-    const [cpf, setCpf] = useState('');
-    const [email, setEmail] = useState('');
-    const [login, setLogin] = useState('');
-    const [senha, setSenha] = useState('');
-    const [senhaC, setSenhaC] = useState('');
+    const[formData, setFormData] = useState({
+        nome: '',
+        cpf: '',
+        email: '',
+        login: '',
+        senha: '',
+        senhaC: ''
+    });
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const user = new User();
-        axios.post(PATH, {
-                nome,
-                cpf,
-                email,
-                login,
-                senha,
-                senhaC
-            }
-        ).then(response => {
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
+
+    const handleSubmit = async (formSubmitEvent) => {
+        formSubmitEvent.preventDefault();
+
+        try {
+
+            const response = await axios.post(PATH, formData);
+            
+            const user = new User();
             user.id = response.data.id;
             user.login = response.data.login;
             user.hash = response.data.hash;
@@ -47,26 +48,42 @@ function RegisterPage({port, currentUser, changeCurrentUser}) {
             user.cpf = response.data.cpf;
             user.email = response.data.email;
             changeCurrentUser(user);
-            console.log(user.toString);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-        .finally(() => {
-            setPostForm(true);
-        });
+
+            console.log(user.toString());
+            window.alert('Cadastro feito com sucesso!');
+            document.location.href = '/';
+            
+        } catch (error) {
+            if(error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            }
+            else if (error.request)
+                console.log(error.request);
+            else 
+                console.log('Error', error.message);
+            
+            console.log(error.config);
+
+            window.alert('Erro ao fazer o cadastro!');
+            document.location.reload();
+        }
     };
 
     useEffect(() => {
         // Requisição para a API do backend
-        axios.get(PATH).then(response => setData(response.data)).catch(error => console.error('Erro ao buscar dados:', error));
+        if(currentUser)
+            document.location.href = '/';
+        else
+            axios.get(PATH).then(response => setData(response.data)).catch(error => console.error('Erro ao buscar dados:', error));
     }, []);
 
     useEffect(() => {
-        if(postForm) {
-            
-        }
-    }, [postForm]);
+        return() => {
+            document.location.href = '/';
+        };
+    }, [currentUser]);
 
     return(
         <> {data && !currentUser
@@ -74,33 +91,33 @@ function RegisterPage({port, currentUser, changeCurrentUser}) {
                 <div className="w3-container w3-padding-64 w3-white w3-border w3-border-gray" id="register">
                     <h1 className="w3-center">{data.message}</h1>
                     <p className="w3-center">Preencha os campos abaixo para se registrar como usuário:</p>
-                    <form className='w3-padding-large' method='POST' autoComplete='off'>
+                    <form id='registerForm' className='w3-padding-large' method='POST' autoComplete='off' onSubmit={handleSubmit}>
                         <p>
                             <label htmlFor="nome">Nome:</label>
-                            <input className="w3-input w3-padding-16" type="text" id="nome" name="nome" onChange={(evt) => setNome(evt.target.value)} required/>
+                            <input className="w3-input w3-padding-16" type="text" id="nome" name="nome" placeholder='Digite o seu nome aqui' value={formData.nome} onChange={handleChange} required/>
                         </p>
                         <p>
                             <label htmlFor="cpf">CPF:</label>
-                            <input className="w3-input w3-padding-16" type="text" id="cpf" name="cpf" onChange={(evt) => setCpf(evt.target.value)} required/>
+                            <input className="w3-input w3-padding-16" type="text" id="cpf" name="cpf" placeholder='Digite o seu CPF aqui' value={formData.cpf} onChange={handleChange} required/>
                         </p>
                         <p>
                             <label htmlFor="email">Email:</label>
-                            <input className="w3-input w3-padding-16" type="email" id="email" name="email" onChange={(evt) => setEmail(evt.target.value)} required/>
+                            <input className="w3-input w3-padding-16" type="email" id="email" name="email" placeholder='Digite o seu Email aqui' value={formData.email} onChange={handleChange} required/>
                         </p>
                         <p>
                             <label htmlFor="login">Login:</label>
-                            <input className="w3-input w3-padding-16" type="text" id="login" name="login" onChange={(evt) => setLogin(evt.target.value)} required/>
+                            <input className="w3-input w3-padding-16" type="text" id="login" name="login" placeholder='Digite o seu login' value={formData.login} onChange={handleChange} required/>
                         </p>
                         <p>
                             <label htmlFor="senha">Senha:</label>
-                            <input className="w3-input w3-padding-16" type="password" id="senha" name="senha" onChange={(evt) => setSenha(evt.target.value)} required/>
+                            <input className="w3-input w3-padding-16" type="password" id="senha" name="senha" placeholder='Digite a sua senha aqui' value={formData.senha} onChange={handleChange} required/>
                         </p>
                         <p>
                             <label htmlFor="senhaC">Confirme a sua Senha:</label>
-                            <input className="w3-input w3-padding-16" type="password" id="senhaC" name="senhaC" onChange={(evt) => setSenhaC(evt.target.value)} required/>  
+                            <input className="w3-input w3-padding-16" type="password" id="senhaC" name="senhaC" placeholder='Digite novamente a sua senha aqui' value={formData.senhaC} onChange={handleChange} required/>  
                         </p>
                         <p>
-                            <button className="w3-button w3-light-grey w3-section" type="submit" formAction={PATH} onSubmit={(evt) => handleSubmit(evt)}>Fazer Cadastro</button>
+                            <button className="w3-button w3-light-grey w3-section" type="submit">Fazer Cadastro</button>
                         </p>
                     </form>
                 </div>
