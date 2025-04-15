d55a9dbc197a7fa46a70f0dacc683edd6742a4ff
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2025-04-15 12:13:34 -0300

    Update ReactCookies.js

--- client/src/components/ReactCookies.js
+++ client/src/components/ReactCookies.js
@@ -6,25 +6,26 @@ function UseCookie(cookieName) {
     const EXPIRATION_DATE = new Date();
     EXPIRATION_DATE.setDate(EXPIRATION_DATE.getDate() + DAYS_TO_EXPIRE);
 
-    const [cookieValue, setCookieValue] = useState("");
+    const getDocumentCookie = () => {
+      const cookie = document.cookie.split("; ").find((row) => row.startsWith(`${cookieName}=`));
+      return cookie ? cookie.split("=")[1] : "";
+    };
+
+    const [cookieValue, setCookieValue] = useState(getDocumentCookie());
 
     useEffect(() => {
-        const cookie = document.cookie
-            .split("; ")
-            .find((row) => row.startsWith(`${cookieName}=`));
-
-            setCookieValue(cookie ? cookie.split("=")[1] : "");
+      setCookieValue(getDocumentCookie());
     }, [cookieName]);
 
     const setCookie = (value) => {
-        document.cookie = `${cookieName}=${value}; expires=${EXPIRATION_DATE.toUTCString()}; path=/`;
-  };
+      document.cookie = `${cookieName}=${value}; expires=${EXPIRATION_DATE.toUTCString()}; path=/`;
+    };
 
     const deleteCookie = () => {
-        document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
-  };
+      document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+    };
 
     return [cookieValue, setCookie, deleteCookie];
-};
+  };
 
 export default UseCookie;
